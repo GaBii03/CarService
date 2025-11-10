30038171de1d7ac3acb3688da522ac3207132463
GaBii03 <gabrielalyavova@abv.bg>
2025-11-10 13:33:18 +0200

    Add initial project structure, static database, and WeatherForecast API

--- CarService.DL/MyStaticDB/StaticDB.cs
+++ CarService.DL/MyStaticDB/StaticDB.cs
@@ -1,12 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CarService.Models.Entities;
 
 namespace CarService.DL.MyStaticDB
 {
     public class StaticDB
     {
-        
+        internal static List<Customer> Customers { get; set; } = new List<Customer>()
+        {
+            new Customer()
+            {
+                Id = 1,
+                Name = "John Doe",
+                Email = "jd@xxx.com"
+            },
+            new Customer()
+            {
+                Id = 2,
+                Name = "Jane Smith",
+                Email = "js@xxx.com"
+            }
+        };
     }
 }
